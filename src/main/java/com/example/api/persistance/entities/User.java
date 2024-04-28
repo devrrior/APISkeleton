d56500b05057ed460db2fe1baed0c66e9185da39
devrrior <devrrior@gmail.com>
2024-04-28 13:39:45 -0600

    feat(Spring-Boot): Initialize project

---
+++ src/main/java/com/example/api/persistance/entities/User.java
@@ -0,0 +1,31 @@
+package com.example.api.persistance.entities;
+
+import com.example.api.persistance.entities.pivots.UserRole;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<UserRole> userRoles;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/example/api/persistance/repositories/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.example.api.persistance.repositories;
+
+import com.example.api.persistance.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+}
